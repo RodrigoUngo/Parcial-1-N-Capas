bfdaafcddfa2139ebb35ad00076fc5596227c570
RodrigoUngo <00075419@uca.edu.sv>
2022-04-23 21:01:57 -0600

    Project init

---
+++ src/main/java/com/grupo6/hoteles/PrimerParcialHotelesGrupo06Application.java
@@ -0,0 +1,13 @@
+package com.grupo6.hoteles;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrimerParcialHotelesGrupo06Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrimerParcialHotelesGrupo06Application.class, args);
+	}
+
+}
